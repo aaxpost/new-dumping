529e580e340462b8216da07792ec2b90a499178b
aleks <aaxpost@gmail.com>
2023-05-31 15:40:01 +0300

    3105 enf of day

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ ini_set('display_errors', 'on');
 //Загрузка библиотеки для парсинга
 require_once ('./vendor/autoload.php');
 use DiDom\Document;
+use Didom\Query;
 
 //Загрузка функции
 require ('function/curlFunc.php');
@@ -21,11 +22,28 @@ require ('function/curlFunc.php');
 $url = 'https://dnepr-traktor.net/ua/p509617504-izmelchitel-vetok-pod.html';
 $siteFile = curlFunc($url);
 
-$siteDoc = new Document('page.html', true);
+$siteDoc = new Document($siteFile);
+$elem = $siteDoc->find('.b-product-cost__price');
 
-$result = $siteDoc->first('div');
+//var_dump($block);
+//$price = $elem->text();
+//var_dump($price);
+//b-product-cost
+/*
+if ($elem->has('span')) {
+  echo "yes elem";
+  $block = $elem->find('span');
+}
+var_dump($block);
+*/
 
-//var_dump($result);
-echo $result->text();
+
+$document = new Document('page.html', true);
+var_dump($document);
+$block = $document->first('#block');
+$elem = $block->first('.elem');
+echo $elem->text();
+
+
 
 
