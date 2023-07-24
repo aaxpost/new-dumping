05d385eea737f2d5087fd35b4fc30263d11f2391
aleks <aaxpost@gmail.com>
2023-07-24 15:43:20 +0300

    добавил считывание из гугл таблиц

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ var_dump($block);
 
 $document = new Document('page.html', true);
 //var_dump($document);
-//Регулярная цена на проме
+//Регулярная цена на проме Рабочий код
 $elem = $document->find('div.b-product-cost');
 $elem = $document->first('*[^data-=product_price]');
 echo $elem->text();
@@ -61,5 +61,21 @@ echo var_export($arr);
 echo '</pre>';
 */
 
+//Получаем данные со страницы гугл лист
+//Для этого разбираем ссылку регулярными выражениями
+$href = 'https://docs.google.com/spreadsheets/d/1ByccFLnRlHkoZeWtoVum3fFzWU5LC1Lk87o5ZaC2w2c/edit#gid=1158696318';
+$patterId = '#d\/(.+)\/.+gid\=([0-9]+)$#';
+preg_match($patterId, $href, $matches);
+$id = $matches[1];
+$gid = $matches[2];
+$csv = file_get_contents('https://docs.google.com/spreadsheets/d/' . $id . '/export?format=csv&gid=' . $gid);
+$csv = explode("\r\n", $csv);
+$array = array_map('str_getcsv', $csv);
+//Блок вывода данных при отладке
+/*
+echo '<pre>';
+echo var_export($array);
+echo '</pre>';
+*/
 
 
